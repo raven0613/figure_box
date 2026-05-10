80fcc4755fb95f267e07f109df40fa707a34591e
raven0613 <reevan0613@gmail.com>
2026-05-10 19:31:19 +0800

    chore: set vite base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '.',
+  base: '/figure_box/',
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src'),
